be756c4d9e5f6832d0474da67ea4d4b23e04c28e
Iris Xu <xuiris97@gmail.com>
2018-11-29 00:33:17 -0800

    Finished deposit, working on topup

--- src/customerInterface.java
+++ src/customerInterface.java
@@ -67,8 +67,9 @@ public class customerInterface {
 		case "deposit":
 			deposit();
 			break;
-//		case "top up":
-//			topUp(id);
+		case "top up":
+			topUp();
+			break;
 //		case "withdraw":
 //			withdraw(id);
 //		case "purchase":
@@ -128,11 +129,25 @@ public class customerInterface {
 	private void deposit() {
 		try {
 			// Ask user for account they want to transact on
-			int aid = chooseAccount();
-			if (aid == 0) {
-				System.out.println("Error when choosing account to deposit to.");
+			int count = 0;
+			int aid = 0;
+			Account a = new Account();
+			while (count < 3) {
+				aid = chooseAccount();
+				if (aid == 0) {
+					System.out.println("Error when choosing account to deposit to.");
+					return;
+				}
+				// Pull account, place in Account object, check if its savings or checkings
+				a = Account.getAccount(conn, aid);
+				if (a.type.equals("Savings") || a.type.equals("Student-Checking") || a.type.equals("Interest-Checking")) break;
+				System.out.println("Please choose only Savings or Checkings.");
+				count += 1;
+			}
+			if (count > 2) {
+				System.out.println("Failed to choose valid account.");
 				return;
-			}			
+			}
 			
 			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 			System.out.println("How much would you like to deposit?");
@@ -144,9 +159,11 @@ public class customerInterface {
 				System.out.println("Not a number");
 			}
 			
-			// Pull account, place in Account object
-			Account a = Account.getAccount(conn, aid);
-			// Modify object to mirror deposit
+			if (amt < 0) {
+				System.out.println("Cannot deposit negative amount.");
+				return;
+			}
+			
 			a.balance += amt;
 			// Update this in the DB using account object.
 			if (a.updateAccountDB(conn)) {
@@ -155,6 +172,71 @@ public class customerInterface {
 		} catch(Exception e){
 			e.printStackTrace();
 			System.out.println("Error depositing into account");
+		}
+	}
+	
+	private void topUp() {
+		try {
+			// Ask user for account they want to transact on
+			int count = 0;
+			int pid = 0;
+			Account pa = new Account();
+			while (count < 3) {
+				pid = chooseAccount();
+				if (pid == 0) {
+					System.out.println("Error when choosing account to topup.");
+					return;
+				}
+				
+				// Check if this is a linked pocket account.
+				try {
+					Statement stmt = conn.createStatement();
+					String qry = "SELECT * from LinkedPockets p where p.pid = " + pid;
+					ResultSet rs = stmt.executeQuery(qry);
+					
+					if (rs.next()) {
+						pa = Account.getAccount(conn, pid);
+						break;
+					} else { 
+						System.out.println("This is not a linked pocket account.");
+					}
+				} catch(SQLException se){
+					se.printStackTrace();
+				}
+				
+				System.out.println("Please choose only a pocket account.");
+				count += 1;
+			}
+			if (count > 2) {
+				System.out.println("Failed to choose valid account.");
+				return;
+			}
+			
+			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("How much would you like to top up into the linked Pocket account?");
+			int amt = 0;
+			try {
+				amt = Integer.parseInt(input.readLine());
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+				System.out.println("Not a number");
+			}
+			
+			if (amt < 0) {
+				System.out.println("Cannot topup negative amount.");
+				return;
+			}
+			
+			pa.balance += amt;
+			
+			// Update this in the DB using account object.
+			if (pa.updateAccountDB(conn)) {
+				// Add transaction.
+			} 
+			
+		} catch(Exception e){
+			e.printStackTrace();
+			System.out.println("Error topping up account");
 		}
 	}
 }
